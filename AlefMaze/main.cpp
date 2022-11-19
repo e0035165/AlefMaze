3159a4b9e395075b913743c329fcc56893740cf0
Sathyasai <eeenginmaster95@gmail.com>
2022-11-19 09:38:08 +0800

    Initial Commit

---
+++ AlefMaze/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AlefMaze
+//
+//  Created by Sart Way on 19/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
